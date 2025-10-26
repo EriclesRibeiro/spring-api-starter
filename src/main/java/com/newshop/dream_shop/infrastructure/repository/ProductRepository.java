c723f05484356bc8be124601a6dd68c2e9d54165
EriclesRibeiro <ericles00@hotmail.com>
2025-10-26 13:19:13 -0300

    feat: added new methods on product repository

--- src/main/java/com/newshop/dream_shop/infrastructure/repository/ProductRepository.java
+++ src/main/java/com/newshop/dream_shop/infrastructure/repository/ProductRepository.java
@@ -1,4 +1,15 @@
 package com.newshop.dream_shop.infrastructure.repository;
 
-public class ProductRepository {
+import com.newshop.dream_shop.domain.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+    List<ProductEntity> findByNameContainingIgnoreCase(String name);
+
+    List<ProductEntity> findByPriceGreaterThen(BigDecimal price);
+
 }
